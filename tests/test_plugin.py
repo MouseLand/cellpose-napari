37f1b46e463f481bf5c307da6d1e90932e68df11
Peter Sobolewski <pete.sd@gmail.com>
2022-08-23 22:46:18 -0400

    call widget, but don't add it

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -26,7 +26,7 @@ SAMPLE = Path(__file__).parent / "sample.tif"
 
 def test_plugin_widget_added(make_napari_viewer: Callable[..., napari.Viewer]):
     viewer = make_napari_viewer()
-    # widget = widget_wrapper()
+    widget = widget_wrapper()
     # viewer.window.add_dock_widget(widget)
     # assert len(viewer.window._dock_widgets) == 1
     a = 1
