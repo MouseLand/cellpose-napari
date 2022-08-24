1837a916aa9451468009d3271574c2851fd765e1
Peter Sobolewski <pete.sd@gmail.com>
2022-08-24 19:45:47 -0400

    skip the viewer_widget fixture

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from math import isclose
+from typing import Callable
 
 import cellpose_napari
 import napari
@@ -16,49 +17,52 @@ WIDGET_NAME = "cellpose"
 SAMPLE = Path(__file__).parent / "sample.tif"
 
 
-@pytest.fixture
-def viewer_widget(make_napari_viewer, napari_plugin_manager):
-    napari_plugin_manager.register(cellpose_napari, name=PLUGIN_NAME)
+# @pytest.fixture
+# def viewer_widget(make_napari_viewer, napari_plugin_manager):
+#     napari_plugin_manager.register(cellpose_napari, name=PLUGIN_NAME)
+#     viewer = make_napari_viewer()
+#     _, widget = viewer.window.add_plugin_dock_widget(
+#         plugin_name=PLUGIN_NAME, widget_name=WIDGET_NAME
+#     )
+#     return viewer, widget
+
+
+# def test_adding_widget_to_viewer(viewer_widget):
+#     assert viewer_widget[1].native.parent() is not None
+
+
+def test_basic_function(qtbot, make_napari_viewer: Callable[..., napari.Viewer]):
     viewer = make_napari_viewer()
-    _, widget = viewer.window.add_plugin_dock_widget(
-        plugin_name=PLUGIN_NAME, widget_name=WIDGET_NAME
-    )
-    return viewer, widget
-
-
-def test_adding_widget_to_viewer(viewer_widget):
-    assert viewer_widget[1].native.parent() is not None
-
-
-# def test_basic_function(qtbot, viewer_widget):
-#     viewer, widget = viewer_widget
-#     viewer.open_sample('cellpose-napari', 'rgb_2D')
-#     viewer.layers[0].data = viewer.layers[0].data[0:128, 0:128]
-
-#     #if os.getenv("CI"):
-#     #    return
-#         # actually running cellpose like this takes too long and always timesout on CI
-#         # need to figure out better strategy
-
-#     assert widget.diameter.value == "30"
-    
-#     widget()  # run segmentation with all default parameters
-
-#     def check_widget():
-#         assert widget.cellpose_layers
-
-#     qtbot.waitUntil(check_widget, timeout=30_000)
-#     # check that the layers were created properly
-#     assert len(viewer.layers) == 5
-#     assert "cp_masks" in viewer.layers[-1].name
-
-#     # check that the segmentation was proper, diam ~ 24.1, should yield 10 cells
-#     assert viewer.layers[-1].data.max() == 10
-
-def test_compute_diameter(qtbot, viewer_widget):
-    viewer, widget = viewer_widget
     viewer.open_sample('cellpose-napari', 'rgb_2D')
     viewer.layers[0].data = viewer.layers[0].data[0:128, 0:128]
+
+    _, widget = viewer.window.add_plugin_dock_widget("cellpose-napari", "cellpose")
+    #if os.getenv("CI"):
+    #    return
+        # actually running cellpose like this takes too long and always timesout on CI
+        # need to figure out better strategy
+
+    assert widget.diameter.value == "30"
+    
+    widget()  # run segmentation with all default parameters
+
+    def check_widget():
+        assert widget.cellpose_layers
+
+    qtbot.waitUntil(check_widget, timeout=30_000)
+    # check that the layers were created properly
+    assert len(viewer.layers) == 5
+    assert "cp_masks" in viewer.layers[-1].name
+
+    # check that the segmentation was proper, should yield 11 cells
+    assert viewer.layers[-1].data.max() == 11
+
+def test_compute_diameter(qtbot, make_napari_viewer: Callable[..., napari.Viewer]):
+    viewer = make_napari_viewer()
+    viewer.open_sample('cellpose-napari', 'rgb_2D')
+    viewer.layers[0].data = viewer.layers[0].data[0:128, 0:128]
+
+    _, widget = viewer.window.add_plugin_dock_widget("cellpose-napari", "cellpose")
 
     # check the initial value of diameter
     assert widget.diameter.value == "30"
