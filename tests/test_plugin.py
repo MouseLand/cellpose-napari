fe50117f1fdf91537e439dff3558cb6cf6687fdc
Peter Sobolewski <pete.sd@gmail.com>
2022-08-24 20:13:40 -0400

    Add 3D, skip >1 test on ubuntu

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pathlib import Path
 from math import isclose
 from typing import Callable
@@ -17,33 +18,19 @@ WIDGET_NAME = "cellpose"
 SAMPLE = Path(__file__).parent / "sample.tif"
 
 
-# @pytest.fixture
-# def viewer_widget(make_napari_viewer, napari_plugin_manager):
-#     napari_plugin_manager.register(cellpose_napari, name=PLUGIN_NAME)
-#     viewer = make_napari_viewer()
-#     _, widget = viewer.window.add_plugin_dock_widget(
-#         plugin_name=PLUGIN_NAME, widget_name=WIDGET_NAME
-#     )
-#     return viewer, widget
-
-
-# def test_adding_widget_to_viewer(viewer_widget):
-#     assert viewer_widget[1].native.parent() is not None
-
-
 def test_basic_function(qtbot, make_napari_viewer: Callable[..., napari.Viewer]):
     viewer = make_napari_viewer()
-    viewer.open_sample('cellpose-napari', 'rgb_2D')
+    viewer.open_sample(PLUGIN_NAME, 'rgb_2D')
     viewer.layers[0].data = viewer.layers[0].data[0:128, 0:128]
 
-    _, widget = viewer.window.add_plugin_dock_widget("cellpose-napari", "cellpose")
+    _, widget = viewer.window.add_plugin_dock_widget(PLUGIN_NAME, WIDGET_NAME)
+    assert len(viewer.window._dock_widgets) == 1
+    
     #if os.getenv("CI"):
     #    return
         # actually running cellpose like this takes too long and always timesout on CI
         # need to figure out better strategy
 
-    assert widget.diameter.value == "30"
-    
     widget()  # run segmentation with all default parameters
 
     def check_widget():
@@ -57,24 +44,42 @@ def test_basic_function(qtbot, make_napari_viewer: Callable[..., napari.Viewer])
     # check that the segmentation was proper, should yield 11 cells
     assert viewer.layers[-1].data.max() == 11
 
+@pytest.mark.skipif(sys.platform.startswith('linux'), reason="ubuntu stalls with two cellpose tests")
 def test_compute_diameter(qtbot, make_napari_viewer: Callable[..., napari.Viewer]):
     viewer = make_napari_viewer()
-    viewer.open_sample('cellpose-napari', 'rgb_2D')
+    viewer.open_sample(PLUGIN_NAME, 'rgb_2D')
     viewer.layers[0].data = viewer.layers[0].data[0:128, 0:128]
 
-    _, widget = viewer.window.add_plugin_dock_widget("cellpose-napari", "cellpose")
+    _, widget = viewer.window.add_plugin_dock_widget(PLUGIN_NAME, WIDGET_NAME)
 
     # check the initial value of diameter
     assert widget.diameter.value == "30"
     # run the compute diameter from image function
     # check that the diameter value used for segmentation is correct
-    with qtbot.waitSignal(widget.diameter.changed, timeout=5000) as blocker:
+    with qtbot.waitSignal(widget.diameter.changed, timeout=30_000) as blocker:
         widget.compute_diameter_button.changed(None)
 
     assert isclose(float(widget.diameter.value), 24.1, abs_tol=10**-1)
 
-# @pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
-# def test_sample_data_with_viewer(widget_name, make_napari_viewer):
-#   viewer = make_napari_viewer()
-#   viewer.open_sample('cellpose-napari', 'rgb_3D.tif')
-#   assert len(viewer.layers) == 1
+@pytest.mark.skipif(sys.platform.startswith('linux'), reason="ubuntu stalls with >1 cellpose tests")
+def test_3D_segmentation(qtbot, make_napari_viewer: Callable[..., napari.Viewer]):
+    viewer = make_napari_viewer()
+    viewer.open_sample(PLUGIN_NAME, 'rgb_3D')
+    viewer.layers[0].data = viewer.layers[0].data[0:128, 0:128]
+
+    _, widget = viewer.window.add_plugin_dock_widget(PLUGIN_NAME, WIDGET_NAME)
+
+    widget.process_3D.value = True
+
+    widget()  # run segmentation with all default parameters
+
+    def check_widget():
+        assert widget.cellpose_layers
+
+    qtbot.waitUntil(check_widget, timeout=60_000)
+    # check that the layers were created properly
+    assert len(viewer.layers) == 5
+    assert "cp_masks" in viewer.layers[-1].name
+
+    # check that the segmentation was proper, should yield 7 cells
+    assert viewer.layers[-1].data.max() == 7
